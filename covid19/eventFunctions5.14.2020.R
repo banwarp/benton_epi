6bf2f63fc9c4144d66d57fe5b7488d55b2ae6243
banwarp <43013293+banwarp@users.noreply.github.com>
2020-05-15 09:00:05 -0700

    Add files via upload
    Updated scripts with new functionality.

---
+++ covid19/eventFunctions5.14.2020.R
@@ -0,0 +1,150 @@
+# eventFunctions5.14.2020.R
+
+# changes from massEventFunction5.8.2020.R
+# added transferEventFunction to this script to clean up main script
+
+# changes from studentEventFunction.R
+# added function for super spread event
+
+
+nodeSampleFun <- function(x,nL=nList,nTL=numTList,out=outLogic) {
+  if(out & length(nL[[x]]) == 1) {
+      return(rep(nL[[x]],nTL[[x]]))
+  } else { return(sample(nL[[x]],nTL[[x]],replace=TRUE)) }
+}
+
+destSampleFun <- function(x,nL=nList,nTL=numTList,ntm=nTM,out=outLogic) {
+  if(out) {
+    destSet <- ntm[-which(ntm$node %in% nL[[x]] | ntm$trial != x),(node)]
+    return(sample(destSet,nTL[[x]],replace=TRUE))
+  } else { return(sample(nL[[x]],nTL[[x]],replace=TRUE)) }
+}
+
+# function to change node=dest transfers to different dest
+destChangeFun <- function(n=node,t=trial,g=nodeGroup,ntm){
+  trialNodes <- ntm[which(ntm$trial==t & ntm$nodeGroup==g),(node)]
+  if(length(trialNodes)>2){
+    return(sample(trialNodes[-which(trialNodes==n)],1))  
+  } else if(length(trialNodes)==2) {return(trialNodes[-which(trialNodes==n)])}
+  else {return(trialNodes)}
+}
+
+transferFunction <- function(
+                              ind,
+                              numTList = numInTransferList,
+                              tS = tspan,
+                              nT = numTrials,
+                              nList = nodeAndGroupList,
+                              minProp = inGroupTransferMinProp,
+                              maxProp = inGroupTransferMaxProp,
+                              Emat = E,
+                              nTM = nodeTrialMat,
+                              u0S = u0$S,
+                              outLogic = FALSE
+                            ) {
+  
+  numTList <- numTList[[ind]]
+  nList <- nList[[ind]]
+  minProp <- minProp[[ind]]
+  maxProp = maxProp[[ind]]
+  
+  if(max(unlist(numTList))>0 & (length(nList[[1]])>1 | outLogic)) {
+    # Transfer data frame
+    transferEvents <- data.frame(
+      event = "extTrans",
+      time = unlist(lapply(numTList, function(x) sample(tS,x,replace=TRUE))),
+      node = unlist(lapply(c(1:nT),nodeSampleFun,nL=nList,nTL=numTList,out=outLogic)),
+      dest = unlist(lapply(c(1:nT),destSampleFun,nL=nList,nTL=numTList,ntm=nTM,out=outLogic)),
+      n = 0,
+      proportion = unlist(lapply(numTList,function(x) round(runif(x,min=minProp,max=maxProp),3))),
+      select = ncol(Emat),
+      shift = 0
+    )
+  
+    # changing node=dest transfers to different dest, dropping transfers if there is only 1 node per group
+    sameIndices <- which(transferEvents$node==transferEvents$dest)
+    transferSame <- transferEvents[sameIndices,]
+    transferSame <- as.data.table(transferSame)[,rn:=as.numeric(rownames(transferSame))]
+    transferSame <- merge(transferSame,nTM,by="node")
+    transferSame <- transferSame[order(rn)]
+    newDest <- transferSame[,destChangeFun(node,trial,nodeGroup,ntm=nTM),by=seq_len(nrow(transferSame))]
+    transferEvents[sameIndices,"dest"] <- newDest[,V1]
+    sameIndices <- which(transferEvents$node==transferEvents$dest)
+    if(length(sameIndices)>0) {transferEvents <- transferEvents[-sameIndices,]}
+    
+    return(transferEvents)
+  } else {return(NULL)}
+}
+
+
+studentEventFunction <- function(
+                                  compart,                   # compartment index
+                                  fracTable = studentPropTable,
+                                  nodeList = studentNodes,
+                                  pop = studentPop,
+                                  nT = numTrials,
+                                  enn = N,
+                                  sRD = studentReturnDate,
+                                  sRS = studentReturnSpread
+                                ) {
+  frac <- fracTable[compart, "frac"]
+  numStud <- round(pop*frac,0)
+  nodeTable <- data.frame(table(sample(nodeList,numStud,replace=TRUE)))
+  nodeTable$Var1 <- as.numeric(as.character(nodeTable$Var1))
+  
+  if(nrow(nodeTable) > 0) {
+    trialMat <- rep(0:(nT-1),each=nrow(nodeTable))*enn
+    nodeAssignments <- data.frame(node=trialMat+nodeTable$Var1,number=rep(nodeTable$Freq,nT))
+    
+    eventDF <- data.frame(
+      event = rep("enter",nrow(nodeAssignments)),
+      time = rep(sRD+sample(-sRS:sRS,nrow(nodeTable),replace=TRUE),nT),
+      node = nodeAssignments$node,
+      dest = 0,
+      n = nodeAssignments$number,
+      proportion = 0,
+      select = compart,
+      shift = 0
+    )
+    
+    return(eventDF)
+  } else {return(NULL)}
+}
+
+superFunction <- function(
+                          ind,
+                          IList=parmList$superInfections,
+                          nodeList=sort(sample(1:N,min(N,parmList$superNodes))),
+                          dayList=parmList$superDate,
+                          spreadList=parmList$superSpread,
+                          startDay=startofSimDay,
+                          nT = numTrials,
+                          enn = N
+                          ) {
+  I <- IList[[ind]]
+  nodes <- nodeList[[ind]]
+  day <- dayList[[ind]]
+  spread <- spreadList[[ind]]
+  if(is.character(day)) {
+    day <- as.numeric(as.Date(day)) - as.numeric(as.Date("2020-01-01")) - startDay
+  }
+  nodeTable <- data.frame(table(sample(nodes,I,replace=TRUE)))
+  nodeTable$Var1 <- as.numeric(as.character(nodeTable$Var1))
+  if(nrow(nodeTable) > 0) {
+    trialMat <- rep(0:(nT-1),each=nrow(nodeTable))*enn
+    nodeAssignments <- data.frame(node=trialMat+nodeTable$Var1,number=rep(nodeTable$Freq,nT))  
+    
+    eventDF <- data.frame(
+      event = rep("intTrans",nrow(nodeAssignments)),
+      time = rep(day+sample(-spread:spread,nrow(nodeTable),replace=TRUE),nT),
+      node = nodeAssignments$node,
+      dest = 0,
+      n = nodeAssignments$number,
+      proportion = 0,
+      select = 1,
+      shift = 1
+    )
+    
+    return(eventDF)
+  } else {return(NULL)}
+}
