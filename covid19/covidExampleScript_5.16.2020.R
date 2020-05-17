279740be78fa1284b9954b0e9a8fe9fcbf530354
banwarp <43013293+banwarp@users.noreply.github.com>
2020-05-16 18:49:34 -0700

    Add files via upload

---
+++ covid19/covidExampleScript_5.16.2020.R
@@ -0,0 +1,91 @@
+# covidPassThroughScript.R
+
+# Pass through script for more efficient scenario generation
+
+library(data.table)
+
+setwd("L:/Health/Epidemiology/Banwarth_Epi/covid19/scripts")
+source("covid19_SimInf_5.16.2020.R")
+
+countyDT <- fread("https://raw.githubusercontent.com/banwarp/benton_epi/master/covid19/countyMetaData_2020.05.04.csv")
+
+countyDT[,simID:=paste("covid",Sys.Date(),County,sep=".")]
+countyDT[,I0Pop:= round(.003*Population,0)]
+countyDT[,N:=ceiling(Population/1000)]
+
+#### All County Scenarios - long run time
+
+baseLineList <- vector(mode = "list", length = nrow(countyDT))
+noPoliciesList <- baseLineList
+superSpreaderList <- baseLineList
+
+for(i in 1:nrow(countyDT)){
+  baseLineList[[i]] <- covidWrapper(simID = paste0(countyDT[i,(simID)],".BaseLine"),
+                         trialPop = countyDT[i,(Population)],
+                         I0Pop = countyDT[i,(I0Pop)],
+                         N = countyDT[i,(N)],
+                         titleString = countyDT[i,(County)]
+                       )
+}
+
+for(i in 1:nrow(countyDT)){
+  noPoliciesList[[i]] <- covidWrapper(simID = paste0(countyDT[i,(simID)],".NoPolicies"),
+                          trialPop = countyDT[i,(Population)],
+                          I0Pop = countyDT[i,(I0Pop)],
+                          N = countyDT[i,(N)],
+                          titleString = countyDT[i,(County)],
+                          switchOffPolicies = 1
+                         )
+}
+
+for(i in 1:nrow(countyDT)){
+  superSpreaderList[[i]] <- covidWrapper(simID = paste0(countyDT[i,(simID)],".SuperSpreader"),
+                             trialPop = countyDT[i,(Population)],
+                             I0Pop = countyDT[i,(I0Pop)],
+                             N = countyDT[i,(N)],
+                             titleString = countyDT[i,(County)],
+                             superNodes = min(floor(countyDT[i,(N)]/2),5),
+                             superDate = "2020-11-01",
+                             superInfections = 50,
+                             superSpread=2
+                            )
+}
+
+
+#### Benton County Scenarios
+
+i <- 2
+
+bentonTest <- covidWrapper(simID=paste0(countyDT[i,(simID)],".Test"),
+                   trialPop = countyDT[i,(Population)],
+                   I0Pop = countyDT[i,(I0Pop)],
+                   N = 500,
+                   titleString = countyDT[i,(County)]
+)
+
+bentonStandard <- covidWrapper(simID=paste0(countyDT[i,(simID)],".Standard"),
+                     trialPop = 65000,
+                     I0Pop = round(.0023*65000,0),
+                     N = 500,
+                     maxStudentNodes = 100,
+                     titleString = countyDT[i,(County)]
+)
+
+bentonCounterFactual <- covidWrapper(simID=paste0(countyDT[i,(simID)],".CounterFactual"),
+                         trialPop = 65000,
+                         I0Pop = round(.0023*65000,0),
+                         N = 500,
+                         maxStudentNodes = 100,
+                         isoRate = .5,
+                         switchOffPolicies = 1,
+                         titleString = countyDT[i,(County)]
+)
+
+bentonEnhancedContactTracing <- covidWrapper(simID=paste0(countyDT[i,(simID)],".enhancedContactTracing"),
+                                 trialPop = 65000,
+                                 I0Pop = round(.0023*65000,0),
+                                 N = 500,
+                                 maxStudentNodes = 100,
+                                 isoRate = .125,
+                                 titleString = countyDT[i,(County)]
+)
