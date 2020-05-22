a8f89bf1f5969170cd5b65abc237112a3007b979
banwarp <43013293+banwarp@users.noreply.github.com>
2020-05-22 11:04:10 -0700

    Update drawingBalls.R

--- covid19/drawingBalls.R
+++ covid19/drawingBalls.R
@@ -1,5 +1,7 @@
 # drawingBalls.R
 
+# A simple function to model mixing with a small number of infectious persons and daily random interactions.
+
 drawingBalls <- function(n,k,alpha){
   p <- 0
   for(i in 1:alpha) {
@@ -7,4 +9,14 @@ drawingBalls <- function(n,k,alpha){
   }
   p <- p/choose(n,k)
   return(p)
+}
+
+drawingBallsDistribution <- function(n,k,alpha,N){
+  p <- 0
+  for(i in 1:alpha) {
+    p <- p + choose((n-i),(k-1))
+  }
+  p <- p/choose(n,k)
+  sd <- sqrt(N*p*(1-p))
+  return(list(N*p,sd))
 }
