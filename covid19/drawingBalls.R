3cf8642f04adbd2482839709715123f0d3a218ec
banwarp <43013293+banwarp@users.noreply.github.com>
2020-05-26 14:39:00 -0700

    Update drawingBalls.R

--- covid19/drawingBalls.R
+++ covid19/drawingBalls.R
@@ -1,22 +1,36 @@
 # drawingBalls.R
 
-# A simple function to model mixing with a small number of infectious persons and daily random interactions.
+# Note: This script does not use the built-in choose(n,k) but instead computes n choose k from the sum/difference of logs.
+# This is a bit slower for large n and k, but it can admit much larger n and k then choose(n,k).
 
 drawingBalls <- function(n,k,alpha){
   p <- 0
+  j <- k-1
   for(i in 1:alpha) {
-    p <- p + choose((n-i),(k-1))
+    m <- n-i
+    top <- c((m-j+1):m)
+    sumlogtop <- sum(log(top))
+    bottom <- c(1:j)
+    sumlogbottom <- sum(log(bottom))
+    padd <- exp(sumlogtop-sumlogbottom)
+    p <- p + padd
   }
-  p <- p/choose(n,k)
+  top <- c((n-k+1):n)
+  sumlogtop <- sum(log(top))
+  bottom <- c(1:k)
+  sumlogbottom <- sum(log(bottom))
+  p <- log(p)-(sumlogtop-sumlogbottom)
+  p <- exp(p)
   return(p)
 }
 
 drawingBallsDistribution <- function(n,k,alpha,N){
-  p <- 0
-  for(i in 1:alpha) {
-    p <- p + choose((n-i),(k-1))
-  }
-  p <- p/choose(n,k)
+  p <- drawingBalls(n,k,alpha)
   sd <- sqrt(N*p*(1-p))
-  return(list(N*p,sd))
+  return(list(mean = N*p,stdev = sd))
+}
+
+drawingBallsSamples <- function(n,k,alpha,N,nTrials) {
+  p <- drawingBalls(n,k,alpha)
+  return(rbinom(nTrials,N,p))
 }
