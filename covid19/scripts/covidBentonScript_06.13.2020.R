993bd75c47f5fb0ad702c08e6bd23b849b8f568d
banwarp <43013293+banwarp@users.noreply.github.com>
2020-06-13 17:42:16 -0700

    Update covidBentonScript_06.13.2020.R

--- covid19/scripts/covidBentonScript_06.13.2020.R
+++ covid19/scripts/covidBentonScript_06.13.2020.R
@@ -1,6 +1,3 @@
-# covidBentonScript_06.13.2020.R
-
-# Pass through script for more efficient scenario generation
 
 setwd("L:/Health/Epidemiology/Banwarth_Epi/covid19/scripts")
 source("covid19_SimInf_06.06.2020.R")
@@ -13,6 +10,8 @@ bentonStatusQuo <- covidWrapper(simID=paste0("covid",Sys.Date(),"BentonCounty.St
                                              paraMu = 1.2,
                                              paraSig = 1.25,
                                              monitoringSuccess = .125,
+                                             maxPrev1 = 30,
+                                             maxPrev2 = 60,
                                              massEntryReturnDate = "2020-09-25",      # Date of mass Entry
                                              massEntryReturnSpread = 6,               # Symmetric spread of days to spread out mass entry
                                              massEntryPop = 18000,                    # Mass entry population
@@ -29,12 +28,16 @@ bentonnoOSU <- covidWrapper(simID=paste0("covid",Sys.Date(),"BentonCounty.noOSU"
                             trialPop = 65000,
                             I0Pop = round(.0015*65000,0),
                             N = 500,
+                            maxPrev1 = 30,
+                            maxPrev2 = 60,
                             paraMu = 1.2,
                             paraSig = 1.25,
                             monitoringSuccess = .125,
                             maxMassEntryNodes = 0,
                             titleString = "Benton County: No OSU"
 )
+
+rm(bentonnoOSU)
 
 bentonCounterFactual <- covidWrapper(simID=paste0("covid",Sys.Date(),"BentonCounty.CounterFactual"),
                                      simDate = "2020-06-01",
@@ -56,6 +59,8 @@ bentonCounterFactual <- covidWrapper(simID=paste0("covid",Sys.Date(),"BentonCoun
                                      switchOffPolicies = 1,
                                      titleString = "Benton County: Counterfactual"
 )
+
+rm(bentonCounterFactual)
 
 bentonPublicHealthChampions <- covidWrapper(simID=paste0("covid",Sys.Date(),"BentonCounty.publicHealthChampions"),
                                            simDate = "2020-06-01",
@@ -79,81 +84,4 @@ bentonPublicHealthChampions <- covidWrapper(simID=paste0("covid",Sys.Date(),"Ben
                                            titleString = "Benton County: PHC"
 )
 
-
-
-r6 <- covidWrapper(simID="NodeGroups",
-                   nodeGroupList = c(rep(1,200),rep(2,150),rep(3,100),rep(4,50)), # List of group IDs for node groups. Default is 1 group of nodes.
-                   parachuteNodeGroups = c(1,2),             # Which nodes groups the parachuters can land in
-                   superInfections = c(100,50),              # Number of infections caused by the super spreader
-                   superNodes = c(20,20),                    # Number of nodes that the super spreader contacts
-                   superNodeGroups = c(3,4),                 # Which node groups the super spreader contacts. Must use list() syntax for multiple events
-                   superDate = c("2020-10-01","2021-02-01"), # Date the super spreader lands. Date can also be numeric i.e. 200
-                   superSpread = c(3,5),                     # Symmetric spread in days of super spreader infections
-                   plotGroups = c(1,3),                      # Which node groups to plot
-                   plotSum = FALSE,                          # Whether to sum across all node groups or sum them separately
-                   lString = "Node Group"                    # Title of legend
-)
-
-
-
-
-b1red <- covidWrapper(simID=paste0("covid",Sys.Date(),"benton_Red"),
-                               trialPop = 65000,
-                               I0Pop = round(.0023*65000,0),
-                               N = 500,
-                               maxMassEntryNodes = 100,
-                               superNodes = c(10,5),
-                               superDate = c("2020-10-15","2020-11-07"),
-                               superInfections = c(100,100),
-                               superSpread = c(0,0),
-                               isoRate = .25,
-                               titleString = "Benton County"
-)
-
-b1orange <- covidWrapper(simID=paste0("covid",Sys.Date(),"benton_Orange"),
-                      trialPop = 65000,
-                      I0Pop = round(.0023*65000,0),
-                      N = 500,
-                      maxMassEntryNodes = 100,
-                      superNodes = 5,
-                      superDate = "2020-10-15",
-                      superInfections = 100,
-                      superSpread = 0,
-                      isoRate = .25,
-                      titleString = "Benton County"
-)
-
-b1yellow <- covidWrapper(simID=paste0("covid",Sys.Date(),"benton_Yellow"),
-                         trialPop = 65000,
-                         I0Pop = round(.0023*65000,0),
-                         N = 500,
-                         maxMassEntryNodes = 100,
-                         superNodes = 5,
-                         superDate = "2020-10-15",
-                         superInfections = 50,
-                         superSpread = 0,
-                         isoRate = .25,
-                         titleString = "Benton County"
-)
-
-b1green <- covidWrapper(simID=paste0("covid",Sys.Date(),"benton_Green"),
-                         trialPop = 65000,
-                         I0Pop = round(.0023*65000,0),
-                         N = 500,
-                         maxMassEntryNodes = 100,
-                         superNodes = 5,
-                         superDate = "2020-10-15",
-                         superInfections = 25,
-                         superSpread = 0,
-                        isoRate = .25,
-                        titleString = "Benton County"
-)
-
-b1noGames <- covidWrapper(simID=paste0("covid",Sys.Date(),"benton_noGames"),
-                        trialPop = 65000,
-                        I0Pop = round(.0023*65000,0),
-                        N = 500,
-                        maxMassEntryNodes = 100,
-                        isoRate = .25,
-                        titleString = "Benton County"
-)
+rm(bentonPublicHealthChampions)
